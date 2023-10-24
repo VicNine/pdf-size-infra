3c4b0f89df4f8878c907726fdd15a16b959c80fc
Victor <viccc.z.wang@gmail.com>
2023-10-24 15:39:04 +1100

    update delete stack

--- delete_stacks.sh
+++ delete_stacks.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo "Emptying S3 bucket..."
+aws s3 rm s3://$(aws s3api list-buckets --query "Buckets[?starts_with(Name, 'pdf-size-api-lambda-stack-pdfs3bucket')].Name" --output text) --recursive
+
 echo "Deleting stacks..."
 
 aws cloudformation delete-stack --stack-name pdf-size-api-lambda-stack
